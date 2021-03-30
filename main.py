916a9c2f02787456b4a4bc3a7e51ffc0bc9c49bc
Carlo Andaya <2507017+carloandaya@users.noreply.github.com>
2021-03-30 01:40:01 -0700

    Start main menu.

--- main.py
+++ main.py
@@ -3,28 +3,40 @@ class Person:
         self.firstname = firstname
         self.lastname = lastname
         self.fullname = firstname + ' ' + lastname
+        self.wagers = []
 
-    def __str__(self): 
+    def __repr__(self): 
         return('Hello my name is {} {}'.format(self.firstname, self.lastname))
 
-    def sayHello(self): 
-        print('Hello world!')
-        
+
 class Wager:
-    def __init__(self, party1: Person, party2: Person, amount):
-        self.party1 = party1
-        self.party2 = party2
-        self.amount = amount
+    def __init__(self, stake1, stake2):
+        self.stake1 = stake1
+        self.stake2 = stake2
 
-    def __str__(self):
-        return('Wager between {} and {} for {}'.format(self.party1.fullname, self.party2.fullname, self.amount)) 
-
+    def __repr__(self): 
+        return(f'{self.stake1} {self.stake2}')
 
 
-p = Person('Bart', 'Simpson')
-me = Person('Carlo', 'Andaya')
-w = Wager(p, me, 150)
+if __name__ == '__main__':
+    while True: 
+        try:
+            menu_selection = int(input(
+'''
+What would you like to do?
+1. Create a person
+2. Create a wager
+3. View persons
+4. View wagers
+5. Exit
 
-print(w)
+Enter your selection: 
+'''
+            ))
+        except ValueError:
+            continue
+        else: 
+            print(menu_selection)
+            break
 
 
